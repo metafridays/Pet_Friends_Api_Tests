b8ff0afd83d31788639399a80b45cb221a61af8b
metafridays <metafridays@live.com>
2020-10-11 16:42:46 +0300

    Change settings.py structure

--- settings.py
+++ settings.py
@@ -1,24 +1,24 @@
 # VALID PARAMS
+base_url = 'https://petfriends1.herokuapp.com/'
 valid_email = 'ofbctcujaexsvmihmi@awdrt.com'
 valid_password = '123456'
-base_url = 'https://petfriends1.herokuapp.com/'
 valid_pet_photo = 'images/cat.jpg'
 valid_pet_name = 'Кисонька'
 valid_pet_age = 12
 valid_pet_animal_type = 'Кошка'
 
-# INVALID AND EMPTY PARAMS
+# INVALID PARAMS
 invalid_email = 'ofbctcujaexsvmihmi@awdrt'
-empty_email = ''
 invalid_password = '1234567'
-empty_password = ''
 incorrect_auth_key = {'key': '0'}
-empty_api_key = {'key': ''}
 incorrect_pet_id = 'qwz'
-empty_pet_id = ''
-empty_pet_photo = 'images/empty.jpg'
-empty_filter = ''
 invalid_filter = 'qwe'
 wrong_type_photo = 'images/cat.heic'
 
-
+# EMPTY PARAMS
+empty_email = ''
+empty_password = ''
+empty_api_key = {'key': ''}
+empty_pet_id = ''
+empty_filter = ''
+empty_pet_photo = 'images/empty.jpg'
